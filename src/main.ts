759c552d33759a052a56a3e3eedde70f989a95a5
Bijin Krishn <bijin.krishn@gmail.com>
2026-03-11 11:25:41 +0530

    docs: initialize swagger documention

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,30 @@
 import { ValidationPipe } from '@nestjs/common'
 import { NestFactory } from '@nestjs/core'
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger'
 import { AppModule } from './app.module'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
+
+  app.setGlobalPrefix('api')
+
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
       forbidNonWhitelisted: true,
     }),
   )
+
+  const SWAGGER_DOC_URL = 'api/swagger-doc'
+  const config = new DocumentBuilder()
+    .setTitle('Auth Platform')
+    .setDescription('The auth-platform API description')
+    .setVersion('1.0')
+    .addBearerAuth()
+    .build()
+  const documentFactory = () => SwaggerModule.createDocument(app, config)
+  SwaggerModule.setup(SWAGGER_DOC_URL, app, documentFactory)
+
   await app.listen(process.env.PORT ?? 3000)
 }
 void bootstrap()
