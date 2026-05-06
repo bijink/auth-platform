e83d292a57190dc49435f6b24a87a3427b7bce7b
Bijin Krishn <bijin.krishn@gmail.com>
2026-05-06 15:46:35 +0530

    feat: add pagination provider

--- src/app.module.ts
+++ src/app.module.ts
@@ -15,6 +15,7 @@ import { AuthInfrastructureModule } from './auth/auth-infrastructure.module'
 import { AuthModule } from './auth/auth.module'
 import { AllExceptionsFilter } from './common/filter'
 import { RequestContextMiddleware } from './common/middleware'
+import { PaginationModule } from './common/pagination/pagination.module'
 import envValidation from './config/env.validation'
 import { AlsModule } from './infra/als/als.module'
 import { PrismaModule } from './infra/prisma/prisma.module'
@@ -47,6 +48,7 @@ import { UserModule } from './user/user.module'
     }),
     ScheduleModule.forRoot(),
     AlsModule,
+    PaginationModule,
   ],
   controllers: [AppController],
   providers: [
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -7,6 +7,7 @@ import {
 } from '@nestjs/common'
 import * as argon from 'argon2'
 import { Prisma } from 'generated/prisma/client'
+import { PaginationProvider } from 'src/common/pagination/pagination.provider'
 import { PrismaService } from 'src/infra/prisma/prisma.service'
 import { OtpService } from 'src/otp/otp.service'
 import { TokenService } from 'src/token/token.service'
@@ -24,10 +25,19 @@ export class UserService {
     private readonly prisma: PrismaService,
     private readonly otpService: OtpService,
     private readonly tokenService: TokenService,
+    private readonly paginate: PaginationProvider,
   ) {}
 
   async findAllUsers() {
-    return await this.prisma.user.findMany({ omit: { password: true } })
+    // return await this.prisma.user.findMany({ omit: { password: true } })
+    return await this.paginate.paginateQuery(
+      this.prisma.user,
+      {
+        limit: 10,
+        page: 1,
+      },
+      { omit: { password: true } },
+    )
   }
 
   async createUser(
